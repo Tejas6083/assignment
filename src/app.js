25dcf7fe2ef94c1aa5d9db19d7ea83f7db7bfd97
unknown <tejas6083@gmail.com>
2021-08-16 13:09:07 +0530

    cors removed

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-var cors = require('cors');
+// var cors = require('cors');
 require("./db/mongoose");
 
 const user = require("./routes/UserRoute");
